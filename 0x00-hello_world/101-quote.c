432cfa44dc1b8a5684889c8b02e576b4fdc62b17
mickey2124 <mikiyasmamush63@gmail.com>
2023-02-16 15:17:25 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-#include <stdio.h>
-
+#include <unistd.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 1 (Success)
- *     */
-
-int write(int filedes, const char *buf, unsigned int nbyte);
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
+ */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			write(STDOUT_FILENO, "and that piece of art is useful
-								\" - Dora Korpar, 2015-10-19\n", 59);
-				return (1);
+	 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	  return (1);
 }
