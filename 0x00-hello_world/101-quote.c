a1fe93b4e493089ec41e870315a55b020bc0506a
mickey2124 <mikiyasmamush63@gmail.com>
2023-02-16 15:11:18 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <stdio.h>
+
 /**
  *  * main - Entry point
  *   *
  *    * Return: Always 1 (Success)
  *     */
+
 int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-			return (1);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+			write(STDOUT_FILENO, "and that piece of art is useful
+								\" - Dora Korpar, 2015-10-19\n", 59);
+				return (1);
 }
