ef1f48483ed6ed8060a4cb79d636619dc34a21c2
mickey2124 <mikiyasmamush63@gmail.com>
2023-02-16 12:38:19 -0800

    0-preprocessor

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,12 @@
+julien@ubuntu:~/c/0x00$ cat main.c 
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	    return (0);
+}
