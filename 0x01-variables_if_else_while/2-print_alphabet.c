9729a8973da0d0bc8eb8a6414b9bb0d5ccde2ddf
mickey2124 <mikiyasmamush63@gmail.com>
2023-02-17 10:44:46 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,12 @@
  */
 int main(void)
 {
-		char c = 'a';
-
-			while (c <= 'z')
-					{
-								putchar(c);
-										c++;
-											}
-				putchar('\n');
-					return (0);
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
 }
