0ce1b4e612eedad1576f0c0b76a5b33f2b8a2612
mickey2124 <mikiyasmamush63@gmail.com>
2023-02-17 11:48:09 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,52 +12,28 @@ int k = '0';
 while (i <= '7')
 {
 while (j <= '8')
-
 {
-
 while (k <= '9')
-
 {
-
 if (i < j && j < k)
-
 {
-
 putchar(i);
-
 putchar(j);
-
 putchar(k);
-
 if (!(i == '7' && j == '8' && k == '9'))
-
 {
-
 putchar(',');
-
 putchar(' ');
-
 }
-
 }
-
 k++;
-
 }
-
 k = '0';
-
 j++;
-
 }
-
 j = '0';
-
 i++;
-
 }
-
 putchar('\n');
-
 return (0);
 }
