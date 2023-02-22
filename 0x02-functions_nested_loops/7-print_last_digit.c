17ceeb8a78d579a96059cc82301aca4e6507e084
mickey2124 <mikiyasmamush63@gmail.com>
2023-02-22 06:05:18 -0800

    alx-low_level_programming

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,13 +5,14 @@
  * @n: The number to be treated.
  * Return: Value of the last digit of number.
  */
+
 int print_last_digit(int n)
 {
 	int last;
-	last = n %10;
+	last = n % 10;
 	if (last < 0)
 {
-	last = last *-1;
+	last = last * -1;
 }
 
 	_putchar(last + '0');
