559b5373f777f0074a60a0c03df49abf3673d26e
mickey2124 <mikiyasmamush63@gmail.com>
2023-02-24 11:25:34 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,26 +9,15 @@ void more_numbers(void)
 	int j, i, d, u;
 
 	for (j = 0; j < 10; j++)
-
-{
-					
+{			
 	for (i = 0; i < 15; i++)
-
 {
-
 	d = i / 10;
-
 	u = i % 10;
-
 	if (d != 0)
-
 	_putchar(d + '0');
-
 	_putchar(u + '0');
-
 }
-
-_putchar('\n');
-
+	_putchar('\n');
 }
 }
