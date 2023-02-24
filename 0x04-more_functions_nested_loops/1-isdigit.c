b65d383c2524dec36ea1b15192f3f1f3f8d6c800
mickey2124 <mikiyasmamush63@gmail.com>
2023-02-24 11:07:10 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,9 +6,9 @@
  *
  * Return: 1 if @c is a digit, 0 if not.
  */
+
 int _isdigit(int c)
 {
-
 	if (c >= '0' && c <= '9')
 
 		return (1);
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,11 +4,16 @@
  * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
  *
  */
+
 void print_numbers(void)
-{
+{	
+
 	int i;
+
 	for (i = '0'; i <= '9'; i++)
 
-		_putchar(i);
+	_putchar(i);
+{
 	_putchar('\n');
+}
 }
