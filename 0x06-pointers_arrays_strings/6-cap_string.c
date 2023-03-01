8268d6aea4abd6fdf700fe828cf97eae98ea48c5
mickey2124 <mikiyasmamush63@gmail.com>
2023-03-01 13:55:24 -0800

    miki

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ while (str[index])
 while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
 
-if (st[index - 1] == '' ||
+if (str[index - 1] == ' ' ||
 str[index - 1] == '\t' ||
 str[index - 1] == '\n' ||
 str[index - 1] == ',' ||
