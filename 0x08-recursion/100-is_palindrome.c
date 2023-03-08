2716a923fb1d0498585b6b89fab3eee2a03d566b
Mickey2124 <mikiyasmamush63@gmail.com>
2023-03-08 09:23:34 -0800

    +251916002668

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,16 +1,48 @@
 #include "main.h"
+int lengthc(char *s);
+int palindrome(char str[], int st, int end);
 /**
- * is_palindrome - find a palindrome string
- * @s: string for check
- *
- * Return: 1 if is palindrome, otherwise 0
+ * is_palindrome - Entry Point
+ * @s: input
+ * Return: 0
  */
 int is_palindrome(char *s)
 {
-int i;
-i = 0;
+	int len;
 
-if (*s == i)
-return (0);
-return (1);
+	len = lengthc(s);
+
+	if (len == 0)
+		return (1);
+	return (palindrome(s, 0, len - 1));
+}
+
+/**
+ * lengthc - finds the length count
+ * @s: input
+ * Return: length size
+ */
+int lengthc(char *s)
+{
+	if (*s != '\0')
+		return (1 + lengthc(s + 1));
+	return (0);
+}
+
+/**
+ * palindrome - checks if start and end of string matches
+ * @str: string
+ * @st: start of string 0
+ * @end: end of string from is_palindrome, from lengthc
+ * Return: if str is a palindrome
+ */
+int palindrome(char str[], int st, int end)
+{
+	if (st >= end)
+		return (1);
+	if (str[st] != str[end])
+		return (0);
+	if (st <= end || st < end + 1)
+		return (palindrome(str, st + 1, end - 1));
+	return (1);
 }
