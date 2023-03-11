88875e1fff48c21f8d9b75e91e1d58d0eae58f98
Mickey2124 <mikiyasmamush63@gmail.com>
2023-03-11 09:50:38 -0800

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,66 +1,47 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
-
 /**
- * coinConverter - Helper function that does all the mathematics
- * @i: Passed in variable from main for calculations
- * Return: The number of coins needed minimum for the passed in variable
- */
-int coinConverter(int i)
-{
-int count = 0;
-
-while (i != 0)
-{
-if (i % 10 == 9 || i % 10 == 7)
-i -= 2;
-else if (i % 25 == 0)
-i -= 25;
-else if (i % 10 == 0)
-i -= 10;
-else if (i % 5 == 0)
-i -= 5;
-else if (i % 2 == 0)
-{
-if (i % 10 == 6)
-i -= 1;
-else
-i -= 2;
-}
-else
-i -= 1;
-count++;
-}
-return (count);
-}
-
-/**
- * main - Takes in exactly one argument for minimum coin count
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
+ * main - function 
+ *@argc: length of argv
+ *@argv: number of argument
+ *Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-int i, coin;
+/*Declaring variables*/
+int position, total, change, aux;
+int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
 
-coin = 0;
+position = total = change = aux = 0;
 
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-i = atoi(argv[1]);
-if (i < 0)
-printf("0\n");
-else
+
+total = atoi(argv[1]); /*Covert str to int*/
+
+if (total <= 0)
 {
-coin = coinConverter(i);
-printf("%d\n", coin);
+printf("0\n");
+return (0);
 }
+
+/*Declaring While*/
+
+while (coins[position] != '\0')
+
+{
+if (total >= coins[position])
+{
+aux = (total / coins[position]);
+change += aux;
+total -= coins[position] * aux;
+}
+position++;
+}
+
+printf("%d\n", change);
 return (0);
 }
