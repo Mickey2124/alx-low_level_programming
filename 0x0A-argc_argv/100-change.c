4b4533affda6ed202e236450344130316972ea3c
Mickey2124 <mikiyasmamush63@gmail.com>
2023-03-11 08:46:48 -0800

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,21 +11,21 @@
  */
 int coinConverter(int i)
 {
-		int count = 0;
+int count = 0;
 
-			while (i != 0)
-					{
-								if (i % 10 == 9 || i % 10 == 7)
-												i -= 2;
-										else if (i % 25 == 0)
-														i -= 25;
-												else if (i % 10 == 0)
-																i -= 10;
-														else if (i % 5 == 0)
-																		i -= 5;
-																else if (i % 2 == 0)
-																			{
-																							if (i % 10 == 6)
+while (i != 0)
+{
+if (i % 10 == 9 || i % 10 == 7)
+i -= 2;
+else if (i % 25 == 0)
+i -= 25;
+else if (i % 10 == 0)
+i -= 10;
+else if (i % 5 == 0)
+i -= 5;
+else if (i % 2 == 0)
+{
+if (i % 10 == 6)
 																												i -= 1;
 																										else
 																															i -= 2;
