2830b3b6cc07287bee10e0a017c61f7c9eac0b49
Mickey2124 <mikiyasmamush63@gmail.com>
2023-03-13 13:45:54 -0700

    miki

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -5,25 +5,26 @@
  *   * @s: string to evaluate
  *    *
  *     * Return: number of words
- *      */
+        */
 int count_word(char *s)
 {
 	int flag, c, w;
 
+	
 		flag = 0;
-			w = 0;
+		w = 0;
 
-				for (c = 0; s[c] != '\0'; c++)
-					{
-						if (s[c] == ' ')
-									flag = 0;
-							else if (flag == 0)
-									{
-										flag = 1;
-		w++;
-			}
-							}
-				return (w);
+			for (c = 0; s[c] != '\0'; c++)
+{
+					if (s[c] == ' ')
+							flag = 0;
+						else if (flag == 0)
+{
+							flag = 1;
+							w++;
+}
+}
+		return (w);
 }
 
 /**
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,21 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-/*
- * free_grid - frees 2d array
- * @grid: 2d grid
- * @height: height dimension of grid
- * Description: frees memory of grid
- * Return: nothing
+
+/**
+ * free_grid - free a 2-dimensional array created by alloc_grid
+ * @grid: a pointer to the first column of the first row of the array
+ * @height: the number of rows in the array
  *
+ * Return: void
  */
 void free_grid(int **grid, int height)
 {
-int i;
-
-for (i = 0; i < height; i++)
-{
-free(grid[i]);
-}
-free(grid);
+	while (height)
+		free(grid[--height]);
+	free(grid);
 }
