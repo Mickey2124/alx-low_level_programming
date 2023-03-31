471e2655fdd5f2dd949408fe0fb589aa835de2f9
Mickey2124 <mikiyasmamush63@gmail.com>
2023-03-31 10:45:06 -0700

    miki

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,16 +7,16 @@
 **/
 size_t print_list(const list_t *h)
 {
-		size_t n = 0;
+	size_t n = 0;
 
-			while (h)
-					{
-								if (!h->str)
-												printf("[0] (nil)\n");
-										else
-														printf("[%u] %s\n", h->len, h->str);
-												h = h->next;
-														n++;
-															}
-				return (n);
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
+	}
+	return (n);
 }
